83c0c13e54bd94511df49c9a4fa0196d04c41df6
Jonathan Lehr <jonathan@aboutobjects.com>
2017-08-28 14:06:03 -0400

    add Shopping and MyTool xcodeprojs

---
+++ MyTool/UnitTests/Pet.swift
@@ -0,0 +1,14 @@
+//
+//  Pet.swift
+//  MyTool
+//
+//  Created by Jonathan Lehr on 8/28/17.
+//  Copyright © 2017 About Objects. All rights reserved.
+//
+
+import Foundation
+
+struct Pet {
+    var identifier: Int
+    var name: String
+}
---
+++ MyTool/UnitTests/UnitTests.swift
@@ -0,0 +1,69 @@
+//
+//  UnitTests.swift
+//  UnitTests
+//
+//  Created by Jonathan Lehr on 8/28/17.
+//  Copyright © 2017 About Objects. All rights reserved.
+//
+
+import XCTest
+
+class UnitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        print()
+    }
+    
+    override func tearDown() {
+        print()
+        super.tearDown()
+    }
+    
+    func testLocalVariables() {
+//        var otherNumber: Int
+//        var number = 1
+//        
+//        var word = "Hey!"
+        var word2: String
+        word2 = "Bar"
+        word2.append(" Foo")
+        print(word2)
+        
+        let myString = String(format: "length %d", word2.characters.count)
+        print(myString)
+    }
+    
+    func testLocalConstantsA() {
+        let word = "Yay!"
+        print(word)
+        
+        print("My word is \(word.lowercased())")
+        print("hello world!".capitalized)
+    }
+    
+    func testNumericConversions() {
+        let x = 2
+        let y = 3.5 * Double(x)
+        print(y)
+    }
+    
+    func testWorkWithStructs() {
+        let myPet = Pet(identifier: 101, name: "Fluffy")
+        print(myPet)
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
